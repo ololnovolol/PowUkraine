9024d0d7ab5a0fd6d5a9c765dd19a48bb1abdfe1
ololnovaolol <novakoleg107@gmail.com>
2022-09-20 15:32:26 +0300

    Add toogle eye to login and register in identity

--- IdentityServer/Extentions/LoggerManager.cs
+++ IdentityServer/Extentions/LoggerManager.cs
@@ -10,7 +10,6 @@ namespace IdentityServer.Extentions
 {
     public static class LoggerManager
     {
-        [Obsolete]
         public static IHost SetupLogger(this IHost host)
         {
             using (var scope = host.Services.CreateScope())
@@ -18,14 +17,8 @@ namespace IdentityServer.Extentions
                 var serviceProvider = scope.ServiceProvider;
                 try
                 {
-
-                    var environment = serviceProvider.GetRequiredService<IHostingEnvironment>();
-
-                    if (!environment.IsDevelopment())
-                    {
-                        var context = serviceProvider.GetRequiredService<AuthorizationDbContext>();
-                        DbInitializer.Initialize(context);
-                    }
+                    var context = serviceProvider.GetRequiredService<AuthorizationDbContext>();
+                    DbInitializer.Initialize(context, serviceProvider);
                 }
                 catch (Exception exception)
                 {
@@ -42,13 +35,14 @@ namespace IdentityServer.Extentions
             Activity.DefaultIdFormat = ActivityIdFormat.W3C;
 
             Log.Logger = new LoggerConfiguration()
+                //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                .WriteTo.File("./LogData/Pow_Identity_WebLog.txt", rollingInterval:
+                .WriteTo.File("Pow_Identity_WebLog.txt", rollingInterval:
                     RollingInterval.Day)
                 .CreateLogger();
         }
