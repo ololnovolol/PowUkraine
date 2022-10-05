3f25979e4463dbbc4eb98a0e91e586685e25b8f5
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-10-06 00:29:05 +0300

    Display marks on allMarks page

---
+++ Pow.WebApi/Controllers/MarksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pow.Domain;
+using Pow.Infrastructure.Repositories.Interfaces;
+
+namespace Pow.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarksController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MarksController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMarks()
+        {
+            var marks = await _unitOfWork.Marks.GetAllAsync();
+            return Ok(marks);
+        }
+    }
+}
