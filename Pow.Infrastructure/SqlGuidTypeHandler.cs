2a9869cc1d4f364b8741ebcd982908afb0d076ef
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-10-05 17:14:23 +0300

    Add SqlGuidTypeHandler

---
+++ Pow.Infrastructure/SqlGuidTypeHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace Pow.Infrastructure
+{
+    public class SqlGuidTypeHandler : SqlMapper.TypeHandler<Guid>
+    {
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            parameter.Value = value.ToString();
+        }
+
+        public override Guid Parse(object value)
+        {
+            return new Guid((string)value);
+        }
+    }
+}
