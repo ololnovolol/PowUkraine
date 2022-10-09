73b571e03315b8e5a95bb37379a0388b951bdf0e
ololnovaolol <novakoleg107@gmail.com>
2022-10-10 02:20:41 +0300

    add modal to msg table=)

---
+++ pow-client/src/pages/manageAllMessages/formModal.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import '../../style/forms/form.css';
+
+const CenterBlock = styled.div`
+    width: 50%;
+    height: auto;
+    background: #fff;
+    position: absolute;
+    align-items: center;
+    margin-top: 5rem;
+    margin-bottom: 5rem;
+    left: 25%;
+    padding: 25px 25px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+`;
+
+const BgButton = styled.button`
+    background: #88a87d none repeat scroll 0% 0%;
+    height: 40px;
+    weight: 100px;
+    border-color: #fff;
+    color: #f0a30a;
+    border-radius: 5px;
+    font-size: 16px;
+    margin-top: 1rem;
+    margin-left: 42%;
+    &:hover {
+        color: #fff;
+        background: #6d8764;
+        cursor: pointer;
+    }
+`;
+
+const Block = styled.div`
+    font-size: 1rem;
+    position: relative;
+    top: 2rem;
+    border: 1 rem solid;
+`;
+
+const Form = styled.form`
+  border-radius: 5px;
+  }
+`;
+
+function FormModal(params) {
+    const [msgData, setMsgData] = useState({
+        Title: params.array.title,
+        EventDate: params.array.eventDate,
+        UserId: params.array.userId,
+        PhoneNumber: params.array.phoneNumber,
+        Description: params.array.description,
+        CreatedDate: params.array.createdDate,
+        Marked: params.array.marked,
+        Files: params.array.files,
+    });
+
+    function handle(e) {
+        const newData = { ...msgData };
+        newData[e.target.id] = e.target.value;
+        setMsgData(newData);
+        console.log(newData);
+    }
+
+    return (
+        <div>
+            <Form>
+                <div className="hh">
+                    <h1>Details Message</h1>
+                </div>
+                <Block>
+                    <label>
+                        Title
+                        <input
+                            id="title"
+                            name="title"
+                            value={msgData.Title}
+                            readonly
+                        />{' '}
+                    </label>
+                    <label>
+                        Event Date
+                        <input
+                            id="eventDate"
+                            name="eventDate"
+                            value={msgData.EventDate}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        UserId
+                        <input
+                            id="userId"
+                            name="userId"
+                            value={msgData.UserId}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        Phone Number
+                        <input
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            value={msgData.PhoneNumber}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        Marked
+                        <input
+                            id="marked"
+                            name="marked"
+                            value={msgData.Marked}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        Created Date
+                        <input
+                            id="createdDate"
+                            name="createdDate"
+                            value={msgData.CreatedDate}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        Description
+                        <textarea
+                            id="eventDate"
+                            name="eventDate"
+                            type={'textarea'}
+                            value={msgData.Description}
+                            readonly
+                        />
+                    </label>
+                    <label>
+                        Files
+                        <input
+                            id="files"
+                            name="files"
+                            value={msgData.Files}
+                            readonly
+                        />
+                    </label>
+                </Block>
+            </Form>
+        </div>
+    );
+}
+
+export default FormModal;
--- pow-client/src/pages/manageAllMessages/table.js
+++ pow-client/src/pages/manageAllMessages/table.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import styled from 'styled-components';
+import * as FcIcons from 'react-icons/fc';
 import * as apiService from '../../common/services/apiService';
+import Modal from './modal';
+import FormModal from './formModal';
 
 const Btn = styled.button`
     background: #88a87d none repeat scroll 0% 0%;
@@ -29,20 +32,60 @@ const Btn = styled.button`
     }
 `;
 
+const BtnRed = styled.button`
+    background: #88a87d none repeat scroll 0% 0%;
+    height: 40px;
+    weight: 100px;
+    justify-content: flex;
+    align-items: center;
+    position: relative;
+    max-width: 100%;
+    align-items: center;
+    color: #fff;
+    min-width: 0px;
+    min-height: 0px;
+    border-radius: 5px;
+
+    justify-content: space-between;
+    align-items: center;
+    list-style: none;
+    text-decoration: none;
+    font-size: 18px;
+    &:hover {
+        color: red;
+        background: #6d8764;
+        cursor: pointer;
+    }
+`;
+
 export default function MessageTable({ theadData, tbodyData, setAllUsers }) {
+    const [show, setShow] = useState(false);
+    const [data, setData] = useState([]);
+
     async function UpdateMsg(value) {
         await apiService.changeUserRole(value.email);
         updateData();
     }
 
     async function deleteMsg(value) {
+        console.log(value);
         await apiService.deleteUser(value.email);
         updateData();
     }
 
-    async function updateData() {
-        const messages = await apiService.getAllMessages();
-        setAllUsers(messages);
+    async function updateData(bool, value) {
+        setShow(bool);
+
+        tbodyData.map(e => {
+            if (
+                e.userId === value.userId &&
+                e.title === value.title &&
+                e.description === value.description
+            ) {
+                console.log(e);
+                setData(e);
+            }
+        });
     }
 
     return (
@@ -52,11 +95,9 @@ export default function MessageTable({ theadData, tbodyData, setAllUsers }) {
                     <th>Event Date</th>
                     <th>Title</th>
                     <th>Description</th>
-                    <th>Created Date</th>
                     <th>Has Mark?</th>
-                    <th>User Id</th>
-                    <th>Update Mark </th>
-                    <th>Delete Mark </th>
+                    <th>Edit </th>
+                    <th>Delete </th>
                 </tr>
             </thead>
             <tbody>
@@ -64,21 +105,41 @@ export default function MessageTable({ theadData, tbodyData, setAllUsers }) {
                     return (
                         <tr key={index}>
                             {theadData.map((key, index) => {
-                                return <td key={row[key]}>{row[key]}</td>;
+                                if (key === 'marked') {
+                                    return (
+                                        <td key={row[key]}>
+                                            {row[key] === 0 ? (
+                                                '-'
+                                            ) : (
+                                                <FcIcons.FcCheckmark />
+                                            )}
+                                        </td>
+                                    );
+                                } else if (key === 'userId') {
+                                } else if (key === 'createdDate') {
+                                } else {
+                                    return <td key={row[key]}>{row[key]}</td>;
+                                }
                             })}
                             <td>
                                 <Btn
                                     type="button"
-                                    onClick={() => UpdateMsg(row)}>
-                                    Update
+                                    onClick={() => updateData(true, row)}>
+                                    Details
+                                    <Modal
+                                        title="Details"
+                                        onClose={() => setShow(false)}
+                                        show={show}>
+                                        <FormModal array={data} />
+                                    </Modal>
                                 </Btn>
                             </td>
                             <td>
-                                <Btn
+                                <BtnRed
                                     type="button"
                                     onClick={() => deleteMsg(row)}>
                                     Delete
-                                </Btn>
+                                </BtnRed>
                             </td>
                         </tr>
                     );
