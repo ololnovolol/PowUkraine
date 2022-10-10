650310ba6b2df3d8bd5a23705c00309d9baf0e7a
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-10-10 14:53:54 +0300

    Add description to marks on map

---
+++ Pow.Application/Services/Interfaces/IMarksOnMapService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pow.Application.Models;
+
+namespace Pow.Application.Services.Interfaces
+{
+    public interface IMarksOnMapService
+    {
+        Task<IEnumerable<MarkOnMap>> GetAllAsync();
+    }
+}
---
+++ Pow.Application/Services/MarksOnMapServices.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pow.Application.Models;
+using Pow.Application.Services.Interfaces;
+using Pow.Infrastructure.Repositories.Interfaces;
+
+namespace Pow.Application.Services
+{
+    public class MarksOnMapServices : IMarksOnMapService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MarksOnMapServices(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<MarkOnMap>> GetAllAsync()
+        {
+            var marks = await _unitOfWork.Marks.GetAllAsync();
+
+            var marksOnMap = new List<MarkOnMap>();
+
+            foreach (var mark in marks)
+            {
+                var relatedMessage = await _unitOfWork.Messages
+                    .GetByIdAsync(mark.MessageId.ToString());
+
+                marksOnMap.Add(new MarkOnMap(
+                    mark.Id,
+                    relatedMessage.Title,
+                    relatedMessage.Description,
+                    mark.GpsLatitude,
+                    mark.GpsLongitude));
+            }
+
+            return marksOnMap;
+        }
+    }
+}
